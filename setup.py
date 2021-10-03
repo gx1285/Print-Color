cd40fe18aa67a19cfb6c9103556cd85e8ae64c7d
gx1285 <78240988+gx1285@users.noreply.github.com>
2021-10-03 15:05:15 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Print-Color",
+    name="printtext-color",
     version="0.0.1",
     author="gx1285",
     author_email="runay2342@gmail.com",
