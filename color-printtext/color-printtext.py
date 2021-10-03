49e2cff87d8703465803277c8102ef5d23a16372
gx1285 <78240988+gx1285@users.noreply.github.com>
2021-10-03 18:53:25 +0900

    Update color-printtext.py

--- color-printtext/color-printtext.py
+++ color-printtext/color-printtext.py
@@ -32,6 +32,27 @@ def REDwinprint(value):
   fg_color = 0x0004 | 0x0008
   cutil.SetConsoleAttribute(cutil.stdout_handle,
                            fg_color | info_.wAttributes & 0x0070)
+def Bluewinprint(value):
+  from ctypes import windll, Structure, byref, wintypes
 
+  class cutil:
+      stdout_handle = windll.kernel32.GetStdHandle(-11)
+      GetConsoleInfo = windll.kernel32.GetConsoleScreenBufferInfo
+      SetConsoleAttribute = windll.kernel32.SetConsoleTextAttribute
+ 
+      class console_screen_buffer_info(Structure):
+          _fields_ = [("dwSize", wintypes._COORD),
+                      ("dwCursorPosition", wintypes._COORD),
+                      ("wAttributes", wintypes.WORD),
+                      ("srWindow", wintypes.SMALL_RECT),
+                      ("dwMaximumWindowSize", wintypes._COORD)]
+ 
+
+  info_ = cutil.console_screen_buffer_info()
+  cutil.GetConsoleInfo(cutil.stdout_handle, byref(info_))
+
+  fg_color = 0000ff
+  cutil.SetConsoleAttribute(cutil.stdout_handle,
+                           fg_color | info_.wAttributes & 0000ff)
   print(value)
   cutil.SetConsoleAttribute(cutil.stdout_handle, info_.wAttributes)
