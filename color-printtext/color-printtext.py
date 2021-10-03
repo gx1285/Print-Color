941d25e7247819e8185c4f76afb2ccf408f95124
gx1285 <78240988+gx1285@users.noreply.github.com>
2021-10-03 18:04:10 +0900

    Update color-printtext.py

--- color-printtext/color-printtext.py
+++ color-printtext/color-printtext.py
@@ -4,7 +4,7 @@ def REDprint(value):
   print(RED + value + END)
 def REDwinprint(value):
   from ctypes import windll, Structure, byref, wintypes
-# 必要なAPI (kernel32)をロード
+
   class cutil:
       stdout_handle = windll.kernel32.GetStdHandle(-11)
       GetConsoleInfo = windll.kernel32.GetConsoleScreenBufferInfo
@@ -17,14 +17,13 @@ def REDwinprint(value):
                       ("srWindow", wintypes.SMALL_RECT),
                       ("dwMaximumWindowSize", wintypes._COORD)]
  
-# 現状の色設定を取得
+
   info_ = cutil.console_screen_buffer_info()
   cutil.GetConsoleInfo(cutil.stdout_handle, byref(info_))
 
   fg_color = 0x0004 | 0x0008
   cutil.SetConsoleAttribute(cutil.stdout_handle,
                            fg_color | info_.wAttributes & 0x0070)
-# 標準出力
+
   print(value)
-# もとの色設定に戻す
   cutil.SetConsoleAttribute(cutil.stdout_handle, info_.wAttributes)
