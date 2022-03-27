2a4206e261ad4c504dea16d4a8d99187b5f23124
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-27 18:18:33 +0900

    Changed output text and added update notifications.

--- colorprintcolor/colorprinttext.py
+++ colorprintcolor/colorprinttext.py
@@ -25,6 +25,8 @@ ver = "v1.0.0"
 import requests
 import time
 def vertion_check():
+    """Check to see if a new version has been released.
+    Added in Ver 1.0"""
     ctp_vertion = requests.get('https://raw.githubusercontent.com/gx1285/color-printtext/main/vertion.txt')
     if f"{ver}\n" != ctp_vertion.text:
         print(f"Latest Version{ctp_vertion.text}has been released.\nYour version is\n{ver}So, it needs to be updated.\nPlease update with pip.")
@@ -50,55 +52,58 @@ Added in ver 1.0"""
         print(f"Latest Version{ctp_vertion.text}has been released.\nYour version is\n{ver}So, it needs to be updated.\nPlease update with pip.")
   print(f"Color-PrintText Vertion {ver}")
 def cprint(text=None,color=None):
+  ctp_vertion = requests.get('https://raw.githubusercontent.com/gx1285/color-printtext/main/vertion.txt')
+  if f"{ver}\n" != ctp_vertion.text:
+        print(f"Latest Version{ctp_vertion.text}has been released.\nYour version is\n{ver}So, it needs to be updated.\nPlease update with pip.")
   if color == None:
     raise TypeError('Argument "color" is missing.')
   elif text == None:
     raise TypeError('Argument "text" is missing.')
   elif color == "nomal":
-    print('\033[01m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[01m'+ text +'\033[0m')
   elif color == "translucent":
-    print('\033[02m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[02m'+ text +'\033[0m')
   elif color == "special-character":
-    print('\033[03m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[03m'+ text +'\033[0m')
   elif color == "underline":
-    print('\033[04m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[04m'+ text +'\033[0m')
   elif color == "color-inversion":
-    print('\033[07m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[07m'+ text +'\033[0m')
   elif color == "transparency":
-    print('\033[08m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[08m'+ text +'\033[0m')
   elif color == "strikethrough":
-    print('\033[09m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[09m'+ text +'\033[0m')
   elif color == "special-character2":
-    print('\033[21m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[21m'+ text +'\033[0m')
   elif color == "black":
-    print('\033[30m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[30m'+ text +'\033[0m')
   elif color == "red":
-    print('\033[31m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[31m'+ text +'\033[0m')
   elif color == "green":
-    print('\033[32m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[32m'+ text +'\033[0m')
   elif color == "ocher":
-    print('\033[33m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[33m'+ text +'\033[0m')
   elif color == "blue":
-    print('\033[34m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[34m'+ text +'\033[0m')
   elif color == "purple":
-    print('\033[35m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[35m'+ text +'\033[0m')
   elif color == "turquoise":
-    print('\033[36m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[36m'+ text +'\033[0m')
   elif color == "nomal2":
-    print('\033[37m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[37m'+ text +'\033[0m')
   elif color == "black-background":
-    print('\033[40m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[40m'+ text +'\033[0m')
   elif color == "red-background":
-    print('\033[41m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[41m'+ text +'\033[0m')
   elif color == "green-background":
-    print('\033[42m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[42m'+ text +'\033[0m')
   elif color == "ocher-background":
-    print('\033[43m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[43m'+ text +'\033[0m')
   elif color == "light-blue-background":
-    print('\033[44m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[44m'+ text +'\033[0m')
   elif color == "purple-background":
-    print('\033[45m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[45m'+ text +'\033[0m')
   elif color == "turquoise-background":
-    print('\033[46m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[46m'+ text +'\033[0m')
   elif color == "white-background":
-    print('\033[47m'+'color-printtext v1.0'+'\033[0m')
+    print('\033[47m'+ text +'\033[0m')
