dd1ded3e93f91323e44c86eaa0e905968856b32d
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-03-27 18:08:55 +0900

    24 additional colors.

--- colorprintcolor/colorprinttext.py
+++ colorprintcolor/colorprinttext.py
@@ -30,17 +30,17 @@ def vertion_check():
         print(f"Latest Version{ctp_vertion.text}has been released.\nYour version is\n{ver}So, it needs to be updated.\nPlease update with pip.")
         return
     print("The latest version is installed.({ver})")
-def sleep(value=None):
+def sleep(seconds=None):
   """Sleeps the operation for the specified number of seconds.
 Added in Ver 1.0"""
   ctp_vertion = requests.get('https://raw.githubusercontent.com/gx1285/color-printtext/main/vertion.txt')
   if f"{ver}\n" != ctp_vertion.text:
         print(f"Latest Version{ctp_vertion.text}has been released.\nYour version is\n{ver}So, it needs to be updated.\nPlease update with pip.")
-  if value == None:
+  if seconds == None:
       raise TypeError('The number of seconds of sleep is not specified.')
-  elif type(value) == str:
+  elif type(seconds) == str:
       raise TypeError('an integer is required (got type str)')
-  time.sleep(value)
+  time.sleep(seconds)
 
 def vertion():
   """Shows the current version.
@@ -49,3 +49,56 @@ Added in ver 1.0"""
   if f"{ver}\n" != ctp_vertion.text:
         print(f"Latest Version{ctp_vertion.text}has been released.\nYour version is\n{ver}So, it needs to be updated.\nPlease update with pip.")
   print(f"Color-PrintText Vertion {ver}")
+def cprint(text=None,color=None):
+  if color == None:
+    raise TypeError('Argument "color" is missing.')
+  elif text == None:
+    raise TypeError('Argument "text" is missing.')
+  elif color == "nomal":
+    print('\033[01m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "translucent":
+    print('\033[02m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "special-character":
+    print('\033[03m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "underline":
+    print('\033[04m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "color-inversion":
+    print('\033[07m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "transparency":
+    print('\033[08m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "strikethrough":
+    print('\033[09m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "special-character2":
+    print('\033[21m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "black":
+    print('\033[30m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "red":
+    print('\033[31m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "green":
+    print('\033[32m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "ocher":
+    print('\033[33m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "blue":
+    print('\033[34m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "purple":
+    print('\033[35m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "turquoise":
+    print('\033[36m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "nomal2":
+    print('\033[37m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "black-background":
+    print('\033[40m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "red-background":
+    print('\033[41m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "green-background":
+    print('\033[42m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "ocher-background":
+    print('\033[43m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "light-blue-background":
+    print('\033[44m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "purple-background":
+    print('\033[45m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "turquoise-background":
+    print('\033[46m'+'color-printtext v1.0'+'\033[0m')
+  elif color == "white-background":
+    print('\033[47m'+'color-printtext v1.0'+'\033[0m')
